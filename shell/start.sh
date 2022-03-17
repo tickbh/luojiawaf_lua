cbb449778f22cd0aa9cb203b3c6989ebeb35fc09
tickbh <tickdream125@hotmail.com>
2022-03-17 16:33:31 +0800

    fix start

--- shell/start.sh
+++ shell/start.sh
@@ -1,3 +1,6 @@
+cp -f $PWD/../nginx.conf /usr/local/openresty/nginx/conf/nginx.conf
+echo "copy nginx.conf success"
+
 /usr/local/openresty/nginx/sbin/nginx -c /usr/local/openresty/nginx/conf/nginx.conf
 echo "send start singal"
 sleep 1
--- shell/stop.sh
+++ shell/stop.sh
@@ -1,3 +1,6 @@
+cp -f $PWD/../nginx.conf /usr/local/openresty/nginx/conf/nginx.conf
+echo "copy nginx.conf success"
+
 /usr/local/openresty/nginx/sbin/nginx -s stop -c /usr/local/openresty/nginx/conf/nginx.conf
 echo "send stop singal"
 sleep 1
